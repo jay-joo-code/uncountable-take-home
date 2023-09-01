f6fe9fc4a6a71a7486b45767b97230d346afb1f6
Jay Joo <jj534@cornell.edu>
2023-09-01 16:48:23 -0400

    implement table view

--- src/views/TableView.tsx
+++ src/views/TableView.tsx
@@ -1,5 +1,36 @@
+import { Table } from "@mantine/core";
+import RAW_DATA from "../data.json";
+import { INPUTS, OUTPUTS } from "../constants/data";
+
 function TableView() {
-  return <div>table</div>;
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Timestamp</th>
+          {INPUTS.map((input) => (
+            <th key={input}>{input}</th>
+          ))}
+          {OUTPUTS.map((output) => (
+            <th key={output}>{output}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(RAW_DATA).map(([timestamp, data]) => (
+          <tr key={timestamp}>
+            <td>{timestamp}</td>
+            {INPUTS.map((input) => (
+              <td key={input}>{data.inputs[input]}</td>
+            ))}
+            {OUTPUTS.map((output) => (
+              <td key={output}>{data.outputs[output]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 }
 
 export default TableView;
