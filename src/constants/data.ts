084ec2aa3e69a5ea9f6ca85aa6770f76645a397e
Jay Joo <jj534@cornell.edu>
2023-09-01 16:48:29 -0400

    refactor out constants

---
+++ src/constants/data.ts
@@ -0,0 +1,29 @@
+export const INPUTS = [
+  "Antioxidant",
+  "Carbon Black High Grade",
+  "Carbon Black Low Grade",
+  "Co-Agent 1",
+  "Co-Agent 2",
+  "Co-Agent 3",
+  "Coloring Pigment",
+  "Curing Agent 1",
+  "Curing Agent 2",
+  "Oven Temperature",
+  "Plasticizer 1",
+  "Plasticizer 2",
+  "Plasticizer 3",
+  "Polymer 1",
+  "Polymer 2",
+  "Polymer 3",
+  "Polymer 4",
+  "Silica Filler 1",
+  "Silica Filler 2",
+] as const;
+
+export const OUTPUTS = [
+  "Viscosity",
+  "Cure Time",
+  "Elongation",
+  "Tensile Strength",
+  "Compression Set",
+] as const;
--- src/views/ScatterplotView.tsx
+++ src/views/ScatterplotView.tsx
@@ -3,38 +3,9 @@ import RAW_DATA from "../data.json";
 import styled from "styled-components";
 import { Button, Center, Title } from "@mantine/core";
 import { useState } from "react";
-
-const INPUTS = [
-  "Antioxidant",
-  "Carbon Black High Grade",
-  "Carbon Black Low Grade",
-  "Co-Agent 1",
-  "Co-Agent 2",
-  "Co-Agent 3",
-  "Coloring Pigment",
-  "Curing Agent 1",
-  "Curing Agent 2",
-  "Oven Temperature",
-  "Plasticizer 1",
-  "Plasticizer 2",
-  "Plasticizer 3",
-  "Polymer 1",
-  "Polymer 2",
-  "Polymer 3",
-  "Polymer 4",
-  "Silica Filler 1",
-  "Silica Filler 2",
-] as const;
+import { INPUTS, OUTPUTS } from "../constants/data";
 
 type Inputs = (typeof INPUTS)[number];
-
-const OUTPUTS = [
-  "Viscosity",
-  "Cure Time",
-  "Elongation",
-  "Tensile Strength",
-  "Compression Set",
-] as const;
 
 type Outputs = (typeof OUTPUTS)[number];
 
@@ -138,7 +109,7 @@ function ScatterplotView() {
 const Toolbar = styled.div``;
 
 const ChartContainer = styled.div`
-  height: 80vh;
+  height: 86vh;
   width: 80vw;
 `;
 
